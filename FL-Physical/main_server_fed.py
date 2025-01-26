d3fa2ef41a5807160bcdd7c21543cea4c1bdec03
garrettheston <glh0007@uah.edu>
2025-01-25 18:05:27 -0600

    Configured model to train off of mnist dataset for image processing.

--- FL-Physical/main_server_fed.py
+++ FL-Physical/main_server_fed.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 # Python version: 3.6
 import random
 import matplotlib
@@ -14,7 +14,6 @@ import torch
 from utils.sampling import mnist_iid, mnist_noniid, cifar_iid
 from utils.options import args_parser
 from models.Update import LocalUpdate
-#from models.Nets import MLP, CNNMnist, CNNCifar, ResNetTest
 from models.Fed import FedAvg
 from models.test import test_img
 import torchvision
@@ -30,7 +29,6 @@ from torch.utils.data import DataLoader, random_split
 import torch.optim as optim
 import torchvision
 import time as t
-#from opacus.validators import ModuleValidator
 from torch.utils.data import Dataset, DataLoader
 from models.server_ssh import Connection_handling
 
@@ -42,48 +40,16 @@ import threading
 
 class CustomDataset(Dataset):
     def __init__(self, data_tensor):
-        #self.data = data_tensor[:, :-1]
-        self.data = data_tensor[:, :-1].reshape(-1,1, 9, 100) #[batch_size, channels, height, width]
+        self.data = data_tensor[:, :-1].reshape(-1, 1, 28, 28)  # MNIST image shape (1, 28, 28)
         self.targets = data_tensor[:, -1]
-        print(self.data.shape)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         return self.data[idx], self.targets[idx]
-    
-def IMU_noniid(dataset, num_users,labels):
-    """
-    Sample non-I.I.D client data from IMU dataset 
-    Altered from Mnist_noniid definition
-    :param dataset:
-    :param num_users:
-    :return:
-    """
-    # 60,000 training imgs -->  200 imgs/shard X 300 shards
-    num_shards, num_imgs = 90, 10
-    idx_shard = [i for i in range(num_shards)]
-    dict_users = {i: np.array([]) for i in range(num_users)}
-    idxs = np.arange(num_shards*num_imgs)
-    #labels = dataset.train_labels.numpy()
 
-    # sort labels
-    idxs_labels = np.vstack((idxs, labels))
-    idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
-    idxs = idxs_labels[0, :]
-
-    # divide and assign 2 shards/client
-    for i in range(num_users):
-        rand_set = set(np.random.choice(idx_shard, 30, replace=False))
-        idx_shard = list(set(idx_shard) - rand_set)
-        for rand in rand_set:
-            dict_users[i] = np.concatenate(
-                (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
-        dict_users[i] = [int(x) for x in dict_users[i]]
-    return dict_users
-
-#Define function to adjust Privacy Budget
+# Define function to adjust Privacy Budget
 def adjustPB(PBList, accList):
     print(PBList)
     newAcc = []
@@ -92,23 +58,21 @@ def adjustPB(PBList, accList):
     newAcc.sort()
     i = len(newAcc)
     indexList = []
-    for item in newAcc: 
+    for item in newAcc:
         index = accList.index(item)
-        if (index in indexList):
+        if index in indexList:
             accList[index] = 0
         index = accList.index(item)
         indexList.append(index)
         print(index)
-        newPB[index] = newPB[index] - (0.1*i)
-        #Set some bounds
-        if (newPB[index] > 2):
+        newPB[index] = newPB[index] - (0.1 * i)
+        if newPB[index] > 2:
             newPB[index] = 2
-        if (newPB[index] < 0.7):
+        if newPB[index] < 0.7:
             newPB[index] = 0.7
         i -= 1
     print(newPB)
     return newPB
-        
 
 
 if __name__ == '__main__':
@@ -119,51 +83,44 @@ if __name__ == '__main__':
         print(currentLine)
 
         if currentLine[0] == 'NUM_CLIENTS':
-            NUM_CLIENTS = currentLine[1]
-            NUM_CLIENTS = int(NUM_CLIENTS)
-        
+            NUM_CLIENTS = int(currentLine[1])
+
         if currentLine[0] == 'SERVER_PORT':
-            PORT = currentLine[1]
-            PORT = int(PORT)
+            PORT = int(currentLine[1])
 
         if currentLine[0] == 'SERVER_IP':
             SERVER = currentLine[1]
 
         if currentLine[0] == 'MODELFOLDER':
             MODELFOLDER = currentLine[1] 
-        
-        if currentLine[0] == 'NUM_gl_EPOCHS':
-            NUM_gl_EPOCHS = currentLine[1] 
-            NUM_gl_EPOCHS = int(NUM_gl_EPOCHS)      
 
-        
+        if currentLine[0] == 'NUM_gl_EPOCHS':
+            NUM_gl_EPOCHS = int(currentLine[1])       
+
         lineCount += 1
 
     f.close()
     # parse args
     args = args_parser()
     args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
-    ################## args def for testing
+
     args.num_users = NUM_CLIENTS
     args.epochs = NUM_gl_EPOCHS
-    args.dataset = 'HAR_LS' 
+    args.dataset = 'MNIST'  # Changed dataset to MNIST
     args.model = 'resnet' 
-    args.num_channels = 1 
-    args.bs =128
+    args.num_channels = 1
+    args.bs = 128
 
-    ##################
     training_accuracy_list = []
     training_loss_list = []
 
     # load dataset and split users
-    dataset = torch.load('../../LS_HAR_data.pt')
-    print(dataset.shape)
-    #dataset = dataset.float()
-    dataset = CustomDataset(dataset)
+    dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+    print(len(dataset))
+    dataset = CustomDataset(torch.Tensor(np.array(dataset.data), dtype=torch.float32))
     
-
     total_count = len(dataset)
-    train_count = int(0.05*total_count) # 5%
+    train_count = int(0.05 * total_count)  # 5%
     test_count = total_count - train_count
     random.seed(42)
     torch.manual_seed(42)
@@ -172,25 +129,19 @@ if __name__ == '__main__':
     
     # build model
     if args.model == 'resnet':
-       # net_glob = ResNetTest(torchvision.models.resnet.BasicBlock, [2, 2, 2, 2]).to(args.device)
-       net_glob = torchvision.models.resnet18()
-      # net_glob.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-       net_glob.conv1 = torch.nn.Conv2d(1, 64, (7, 7), (2, 2), (3, 3), bias=False)
-       net_glob.fc = torch.nn.Linear(net_glob.fc.in_features,5)
-       net_glob.to(args.device)
+        net_glob = torchvision.models.resnet18()
+        net_glob.conv1 = torch.nn.Conv2d(1, 64, (7, 7), (2, 2), (3, 3), bias=False)
+        net_glob.fc = torch.nn.Linear(net_glob.fc.in_features, 10)  # MNIST has 10 classes
+        net_glob.to(args.device)
     else:
         exit('Error: unrecognized model')
 
-
     net_glob.load_state_dict(torch.load("models/main_server_fed_overall.pt", map_location=torch.device('cpu')))
-
     net_glob.train()
 
     # copy weights
     w_glob = net_glob.state_dict()
 
-
-    # training
     loss_train = []
     cv_loss, cv_acc = [], []
     val_loss_pre, counter = 0, 0
@@ -199,35 +150,31 @@ if __name__ == '__main__':
     val_acc_list, net_list = [], []
 
     clientAddresses = []
-   
 
-    file1 = open("output_FL_Resnet_HAR.txt", "w") 
+    file1 = open("output_FL_Resnet_MNIST.txt", "w")
 
-    epsList = [ ]
+    epsList = []
 
     if args.all_clients: 
         print("Aggregation over all clients")
         w_locals = [w_glob for i in range(args.num_users)]
-    
-    #host = "10.4.159.106"   # this the address of server computer (not client!!)
+
     host = SERVER
     port = PORT
 
-   # set up TCP socket connection for server 
+    # set up TCP socket connection for server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((host,port))
+    server.bind((host, port))
     server.listen(args.num_users) 
         
     for iter in range(args.epochs):
-
-        #Distribute the model to all clients
         for idx in range(0, args.num_users):
             print("In for loop :)")
             clientsocket, address = server.accept() 
             print("connection from " + address[0] + " accepted.")
             Connection_handling(clientsocket, address)
 
-        #Wait for all client models to be received
+        # Wait for all client models to be received
         modelFolder = MODELFOLDER
         fileCount = 0
         while (fileCount != args.num_users):
@@ -240,16 +187,12 @@ if __name__ == '__main__':
         if not args.all_clients:
             w_locals = []
         m = max(int(args.frac * args.num_users), 1)
-        #Comment out when using adaptive dp
         idxs_users = np.random.choice(range(args.num_users), m, replace=False)
-        #print("Idx List: " , idxs_users)
-        
+
         accuracyList = []
-        #for idx in idxs_users:
         for idx in range(1, args.num_users+1):
-            print(" User: " , idx)
+            print(" User: ", idx)
             
-           # 'fed_{}_{}_{}_C{}_Non_iid{}_DP_3_clients.png'.format(args.dataset)
             File_in_use = True
             while File_in_use:
                 try:
@@ -260,50 +203,37 @@ if __name__ == '__main__':
                     time.sleep(4)
                     File_in_use = True
             net_glob.load_state_dict(checkpoint)
-
-
-           # net_glob.load_state_dict(checkpoint['model_state_dict'])
             localModel = net_glob.state_dict()
 
             if args.all_clients:
                 w_locals[idx] = copy.deepcopy(localModel)
             else:
                 w_locals.append(copy.deepcopy(localModel))
-            #loss_locals.append(copy.deepcopy(loss))
-
-        #print("Epsilon List: ", epsList)
 
         # update global weights
         if args.global_aggr == 'FedAvg':
             w_glob = FedAvg(w_locals)
-            #print('this actually runs')
         else:
             print('something wrong')
-            
-            
+
         # copy weight to net_glob
         net_glob.load_state_dict(w_glob)
-        
-        #save the model
-        torch.save(net_glob.state_dict(), "models/main_server_fed_overall.pt")
 
+        # save the model
+        torch.save(net_glob.state_dict(), "models/main_server_fed_overall.pt")
 
         # print loss and accuracy of current model
         net_glob.eval()
-        acc_train, l = test_img(net_glob,dataset_train, args)
+        acc_train, l = test_img(net_glob, dataset_train, args)
         training_accuracy_list.append(acc_train)
         training_loss_list.append(l)
         print('Accuracy: ', acc_train)
         print('Loss: ', l)
         print(training_accuracy_list)
-        #clearprint(training_loss_list)
-        
-        #Remove all previous models for new ones to come in
+
+        # Remove all previous models for new ones to come in
         for file in os.scandir(modelFolder):
             os.remove(file)
-
-            
-
 
     # Final Connection Handling to terminate clients
     for idx in range(0, args.num_users):  
@@ -316,8 +246,7 @@ if __name__ == '__main__':
 
     # testing
     net_glob.eval()
-
     acc_test, loss_test = test_img(net_glob, dataset_train, args)
 
-    #Close the server
+    # Close the server
     server.close()
--- FL-Physical/staging.py
+++ FL-Physical/staging.py
@@ -1,57 +1,21 @@
-#includes
 import paramiko
 from scp import SCPClient
 import socket
 import time
-#import matplotlib
-#matplotlib.use('Agg')
-#import matplotlib.pyplot as plt
 import copy
 import numpy as np
 from torchvision import datasets, transforms
 import torch
 import torchvision
-from torch.utils.data import Dataset,DataLoader, random_split
+from torch.utils.data import Dataset, DataLoader, random_split
 import torch.nn.functional as F
 from torch import nn
 import random
 
-def test(net_g, data_loader, args):
-    # testing
-    net_g.eval()
-    loss = 0
-    correct = 0
-    with torch.no_grad():  # No need to track gradients during inference
-        for data, target in data_loader:
-            data, target = data.to(args.device), target.to(args.device)
-            log_probs = net_g(data)
-            loss += F.cross_entropy(log_probs, target.long()).item()
-            y_pred = log_probs.argmax(dim=1)
-            correct += y_pred.eq(target).sum().item()
-
-    loss /= len(data_loader.dataset)
-    accuracy = correct / len(data_loader.dataset) * 100
-    print('Metrics: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        loss, correct, len(data_loader.dataset), accuracy))
-
-    return accuracy, loss
-    
-class DatasetSplit(Dataset):
-    def __init__(self, dataset, idxs):
-        self.dataset = dataset
-        self.idxs = list(idxs)
-
-    def __len__(self):
-        return len(self.idxs)
-
-    def __getitem__(self, item):
-        image, label = self.dataset[self.idxs[item]]
-        return image, label
-
+# Custom Dataset for handling MNIST
 class CustomDataset(Dataset):
     def __init__(self, data_tensor):
-        #self.data = data_tensor[:, :-1]
-        self.data = data_tensor[:, :-1].reshape(-1,1, 9, 100) #[batch_size, channels, height, width]
+        self.data = data_tensor[:, :-1].reshape(-1, 1, 28, 28)  # Adjusting for MNIST's 28x28 size and 1 channel
         self.targets = data_tensor[:, -1]
 
     def __len__(self):
@@ -60,16 +24,16 @@ class CustomDataset(Dataset):
     def __getitem__(self, idx):
         return self.data[idx], self.targets[idx]
 
+# Local Update Class
 class LocalUpdate(object):
-    def __init__(self, args, dataset_train=None,dataset_test=None):
+    def __init__(self, args, dataset_train=None, dataset_test=None):
         self.args = args
         self.loss_func = nn.CrossEntropyLoss()
-        self.ldr_train = DataLoader(dataset_train, batch_size=self.args.local_bs, shuffle=True) #local_bs =1 for HAR_LS for now
+        self.ldr_train = DataLoader(dataset_train, batch_size=self.args.local_bs, shuffle=True)
         self.ldr_test = DataLoader(dataset_test, batch_size=args.local_bs, shuffle=False)
 
     def train(self, net):
         net.train()
-        # train and update
         optimizer = torch.optim.SGD(net.parameters(), lr=self.args.lr, momentum=self.args.momentum)
         epoch_loss = []
         for iter in range(self.args.local_ep):
@@ -84,45 +48,38 @@ class LocalUpdate(object):
                 loss = self.loss_func(log_probs, labels)
                 loss.backward()
                 optimizer.step()
-                #if self.args.verbose and batch_idx % 2 == 0:
-                 #   print('Update Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                  #      iter, batch_idx * len(images), len(self.ldr_train.dataset),
-                   #            100. * batch_idx / len(self.ldr_train), loss.item()))
                 batch_loss.append(loss.item())
-            epoch_loss.append(sum(batch_loss)/len(batch_loss))
+            epoch_loss.append(sum(batch_loss) / len(batch_loss))
             print('Local Epoch {} Finished'.format(iter))
             train_accuracy, train_loss = test(net, self.ldr_train, self.args)
         return net.state_dict(), sum(epoch_loss) / len(epoch_loss), epoch_loss
 
+# Test function for evaluating the model
+def test(net_g, data_loader, args):
+    net_g.eval()
+    loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in data_loader:
+            data, target = data.to(args.device), target.to(args.device)
+            log_probs = net_g(data)
+            loss += F.cross_entropy(log_probs, target.long()).item()
+            y_pred = log_probs.argmax(dim=1)
+            correct += y_pred.eq(target).sum().item()
 
-def SendToServer(server, file = "",filepath = "",message = ""):
- #   try:
+    loss /= len(data_loader.dataset)
+    accuracy = correct / len(data_loader.dataset) * 100
+    print('Metrics: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        loss, correct, len(data_loader.dataset), accuracy))
+
+    return accuracy, loss
+
+# Function to send the model to the server
+def SendToServer(server, file="", filepath="", message=""):
     with SCPClient(server.get_transport()) as scp_Client:
         scp_Client.put(file, filepath)
 
-    #serverSocket.send(bytes(message, "utf-8"))
-#    except:
-        #print("SentToServer() Failed.")
-
-Searching_connection = True
-PORT = 4045
-#SERVER = "10.4.159.106"
-#SERVER = "10.4.148.119"
-
-#Create dataset
-dataset = torch.load('LS_HAR_data.pt').float()
-dataset = CustomDataset(dataset)
-
-#Split data
-total_count = len(dataset)
-train_count = int(0.01*total_count) # 5%
-test_count = total_count - train_count # 
-random.seed(42)
-torch.manual_seed(42)
-dataset_train, dataset_test = random_split(dataset, [train_count, test_count])
-
-
-#Defining arguments
+# Arguments for training
 class Args:
     def __init__(self):
         self.local_bs = 128
@@ -131,125 +88,87 @@ class Args:
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.verbose = True
         self.local_ep = 1
-        #self.client_id = 1
-        #self.ServerName = "ServerUsername"
-        #self.ServerPassword = "ServerPassword"
 args = Args()
-
 
 # Read in the config file
 f = open("config.txt", "r")
-
-lineCount = 0
 for line in f:
     currentLine = line.strip('\n').split("=")
-    print(currentLine)
-    
     if currentLine[0] == 'CLIENT_ID':
         CLIENT_ID = currentLine[1]
-        #CLIENT_ID = int(CLIENT_ID)
-        print(currentLine[1])
     if currentLine[0] == 'SERVER_PORT':
-        PORT = currentLine[1]
-        PORT = int(PORT)
-        print(currentLine[1])
+        PORT = int(currentLine[1])
     if currentLine[0] == 'SERVER_IP':
         SERVER = currentLine[1]
-        print(SERVER)
     if currentLine[0] == 'SERVER_NAME':
         SERVER_NAME = currentLine[1]
-        print(currentLine[1])
     if currentLine[0] == 'SERVER_PASS':
         SERVER_PASS = currentLine[1]
-        print(currentLine[1])
     if currentLine[0] == 'SERVER_FILE_LOC':
         SERVER_FILE_LOC = currentLine[1]
-        print(currentLine[1])
-    lineCount += 1
-
 f.close()
 
-while True:
+# Create dataset (MNIST)
+dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
 
+# Split data
+total_count = len(dataset)
+train_count = int(0.01 * total_count)  # 1% for training
+test_count = total_count - train_count
+random.seed(42)
+torch.manual_seed(42)
+dataset_train, dataset_test = random_split(dataset, [train_count, test_count])
 
-    test_loader = DataLoader(dataset_test, batch_size=args.local_bs, shuffle=False)
-    torch.manual_seed(56)
-    
-    # Create an instance of LocalUpdate
-    local_update = LocalUpdate(args, dataset_train, dataset_test)
+# Create an instance of LocalUpdate
+test_loader = DataLoader(dataset_test, batch_size=args.local_bs, shuffle=False)
+local_update = LocalUpdate(args, dataset_train, dataset_test)
 
-    #Create an instance of model architecture
-    net_glob = torchvision.models.resnet18()
-    net_glob.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False) #args.num_channels,num out channels, all others labeled
-    net_glob.fc = torch.nn.Linear(net_glob.fc.in_features, 5) #5 = num features for HAR_LS
-    net_glob.to(args.device)
-##
+# Define the model architecture (ResNet for MNIST)
+net_glob = torchvision.models.resnet18()
+net_glob.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)  # 1 channel for MNIST
+net_glob.fc = torch.nn.Linear(net_glob.fc.in_features, 10)  # 10 classes for MNIST
+net_glob.to(args.device)
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    Searching_connection = True
-    while Searching_connection:
-        try:
-            #connect here ##############
-            #PORT = 4045          
-            #SERVER = "10.4.130.19"
-            client.connect((SERVER, PORT))
-            Searching_connection = False
+# Start the client connection
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Searching_connection = True
+while Searching_connection:
+    try:
+        client.connect((SERVER, PORT))
+        Searching_connection = False
+    except:
+        print("Searching for server system...")
+        time.sleep(2)
 
-        except:
-            print("Searching for server system...")
-            Searching_connection = True
-            time.sleep(2)
-    
-    ## Handle Connection
-    # here is were we have communication with a socket back and forth
-    msg = client.recv(1024)
-    msg_decoded = msg.decode("utf-8")
-    print(msg_decoded)
+# Handle the connection and receive message
+msg = client.recv(1024)
+msg_decoded = msg.decode("utf-8")
+print(msg_decoded)
 
-    if(msg_decoded == "EXIT()"):
-        client.send(bytes("Client-"+CLIENT_ID+" terminated","utf-8"))
-        client.close()
-        exit()
-    
-    client.send(bytes("Client recieved file from sever","utf-8"))
+if(msg_decoded == "EXIT()"):
+    client.send(bytes(f"Client-{CLIENT_ID} terminated", "utf-8"))
     client.close()
-    # we close socket here        
-    
-    ## Model Training
-    time.sleep(5)
-    # Load the model dictionary/parameters
-    print("Loading Model Parameters...")
-    net_glob.load_state_dict(torch.load('main_server_fed.pt'))
-    # Call training function
-    print("\nTraining...")
-    state_dict, avg_loss, lossPerEpoch = local_update.train(net_glob)
-    print("Training Finished")
-    # Save the model dictionary/parameters
-    torch.save(state_dict, 'main_server_fed_'+CLIENT_ID+'.pt')
+    exit()
 
+client.send(bytes("Client received file from server", "utf-8"))
+client.close()
 
+# Model Training
+time.sleep(5)
+# Load model parameters
+print("Loading Model Parameters...")
+net_glob.load_state_dict(torch.load('main_server_fed.pt'))
 
-    ## Send Model
-    # Here is only sending the model back
-    # no sockets
-    username = SERVER_NAME  # username of central server
-    password = SERVER_PASS  # password of central server
-    file_path = SERVER_FILE_LOC
-        
+# Train the model
+print("\nTraining...")
+state_dict, avg_loss, lossPerEpoch = local_update.train(net_glob)
+print("Training Finished")
 
-    server_SSH = paramiko.client.SSHClient()
-    server_SSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    server_SSH.connect(SERVER, username=username, password=password)
-    SendToServer(server=server_SSH,file="main_server_fed_"+CLIENT_ID+".pt",
-                filepath=file_path+"main_server_fed_"+CLIENT_ID+".pt",
-                message="sent file")
+# Save the trained model
+torch.save(state_dict, f'main_server_fed_{CLIENT_ID}.pt')
 
-
-
-
-
-
-
-
-
-
+# Send model back to the server
+server_SSH = paramiko.client.SSHClient()
+server_SSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+server_SSH.connect(SERVER, username=SERVER_NAME, password=SERVER_PASS)
+SendToServer(server=server_SSH, file=f"main_server_fed_{CLIENT_ID}.pt", filepath=SERVER_FILE_LOC + f"main_server_fed_{CLIENT_ID}.pt", message="sent file")
